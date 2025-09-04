4849521112c41a74aaa755b71e6f5d875a0a966e
wolbyte <wolbyte@gmail.com>
2025-09-04 14:30:44 +0330

    chore: remove redundant fields in user model

--- models/user.go
+++ models/user.go
@@ -8,9 +8,6 @@ import (
 
 type User struct {
 	gorm.Model
-	ID           uint      `json:"id" gorm:"uniqueIndex;not null"`
 	PhoneNumber  string    `json:"phone_number" gorm:"uniqueIndex;not null"`
 	RegisteredAt time.Time `json:"registered_at" gorm:"default:CURRENT_TIMESTAMP"`
-	OTP          string
-	OTPExpiry    time.Time
 }
